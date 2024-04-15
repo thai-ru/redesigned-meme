1c7f09435b0151f9f2e1a994587bcbc3f2f0e2ca
thai-ru <robinthairu34@gmail.com>
2024-04-15 22:57:04 +0300

    Initial Commit

---
+++ eBankUI/eBankUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  eBankUIApp.swift
+//  eBankUI
+//
+//  Created by Robinson Thairu on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct eBankUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
